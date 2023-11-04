ec12515220af8b395979da089190697d0c9e2dfc
Elise Ngo <elisetvy@gmail.com>
2023-11-04 11:21:08 -0700

    Rewrite schema and seed

--- friender-schema.sql
+++ friender-schema.sql
@@ -1,23 +1,12 @@
 CREATE TABLE users (
   username VARCHAR(20) PRIMARY KEY,
   password VARCHAR(100) NOT NULL,
-  first_name VARCHAR(20) NOT NULL,
-  last_name VARCHAR(20) NOT NULL,
+  fname VARCHAR(20) NOT NULL,
+  lname VARCHAR(20) NOT NULL,
   email VARCHAR(30) NOT NULL UNIQUE
     CHECK (position('@' IN email) > 1),
-  zip_code VARCHAR(5) NOT NULL,
-  friend_radius INTEGER NOT NULL,
-  hobbies TEXT,
-  interests TEXT
+  photo TEXT,
+  zip VARCHAR(5) NOT NULL,
+  radius INTEGER NOT NULL DEFAULT 25,
+  bio TEXT
 );
-
-CREATE TABLE photos (
-  username VARCHAR(20) NOT NULL
-    REFERENCES users(username),
-  photo_profile VARCHAR(200) NOT NULL DEFAULT 'https://friender-bucket-79.s3.us-west-2.amazonaws.com/default-user.jpg',
-  photo_one VARCHAR(200),
-  photo_two VARCHAR(200),
-  photo_three VARCHAR(200),
-  photo_four VARCHAR(200),
-  photo_five VARCHAR(200)
-)
--- friender-seed.sql
+++ friender-seed.sql
@@ -1,60 +1,20 @@
-INSERT INTO users (username, password, first_name, last_name, email, zip_code, friend_radius, hobbies, interests)
-VALUES ('Jay',
+INSERT INTO users (username, password, fname, lname, email, photo, zip, radius, bio)
+VALUES ('testuser',
         '$2b$12$kqxU1kB58bb4q05U0wAuD.akGDdAJSozxQTrAQsYS.sasz5Ph/TdK',
-        'Jay',
-        'Catsby',
-        'jay@test.com',
+        'test',
+        'user',
+        'test@test.com',
+        'https://media.istockphoto.com/id/155353122/photo/breaded-cat.jpg?s=612x612&w=0&k=20&c=T0xVGbkZIxHm1syWFx_5VxD5AeoTqMJtC8sBa3MDo-Y=',
         '35209',
         25,
-        'swimming',
-        'fishing'
-        ),
-        ('BingsuPaws',
+        'testin around the christmas tree'),
+        ('elise',
         '$2b$12$nh4gnpT6dfHQMRTdd2MWkuJHqsQ6/JDRMW.NdcmMBUXvD.wVRg5x.',
-        'Bingsu',
-        'Tran',
-        'bingsu@test.com',
+        'elise',
+        'verycool',
+        'elise@email.com',
+        'https://i.pinimg.com/736x/24/1f/49/241f49ca612ef379a78fdcf7b8471ada.jpg',
         '79930',
-        25,
-        'hiking',
-        'sunrises'
-        ),
-        ('Goose',
-        '$2b$12$PC1GW0HtWC3DNrHBfMxbfO9LdTXDIhsmO9BMX60ExsDIlv95uRZ26',
-        'Goose',
-        'Ngo',
-        'gooose@test.com',
-        '08332',
-        25,
-        'surfing',
-        'ocean'
-        ),
-        ('Bonksu',
-        '$2b$12$mr9U1mCp5BN/qT53N/o5i.6OW2vqKYQBDSCPIZpLCq0/9p6PExmFa',
-        'Bonksu',
-        'Paws',
-        'bonksu@test.com',
-        '44004',
-        25,
-        'woodworking',
-        'camping'
-        ),
-        ('Cream',
-        '$2b$12$17Xh/68JcU17lyby0tHdEuJt81PlBGKP3QZ27trXcEtWu.P1vhXja',
-        'Cream',
-        'Steele',
-        'cream@test.com',
-        '32034',
-        25,
-        'pinball',
-        'pinball machines'
-        );
-
-INSERT INTO photos(username, photo_profile)
-VALUES('Jay', 'https://people.com/thmb/AcLIEFQcs2R9bzxkdgSznJBKhHQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(999x0:1001x2)/fat-cat-4-5d7ae9c19c4e4e2689897642e7a3363c.jpg'),
-('BingsuPaws', 'https://friender-bucket-79.s3.us-west-2.amazonaws.com/06f0b255-de40-4230-849e-3dfceae3df1f'),
-('Goose', 'https://www.whitemarmotte.com/wp-content/uploads/2020/06/lynx_head.jpg'),
-('Bonksu', 'https://www.livingdesert.org/site/assets/files/4764/mountain_lionheader.1000x800se.png'),
-('Cream', 'https://www.thesprucepets.com/thmb/17UY4UpiMekV7WpeXDziXsnt7q4=/1646x0/filters:no_upscale():strip_icc()/GettyImages-145577979-d97e955b5d8043fd96747447451f78b7.jpg');
-
+        100,
+        'deep sea diving');
 
