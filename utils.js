f78ff92acb6bde26b1d88aa0117e53cbdd679563
Elise Ngo <elisetvy@gmail.com>
2023-11-05 11:25:40 -0800

    Write functions to get distance between 2 ZIP codes and to check if a someone is within a user's friend radius

--- utils.js
+++ utils.js
@@ -5,9 +5,23 @@ require("dotenv").config();
 /** Convert zip code to lat/lng. */
 async function convertZip(zip) {
   const resp = await fetch(`https://www.mapquestapi.com/geocoding/v1/address?key=${process.env.MAPQUEST_KEY}&location=${zip}`);
-  const data = (await resp.json()).results[0].locations[0].latLng
+  const data = (await resp.json()).results[0].locations[0].latLng;
 
   return data.lat.toString() + ',' + data.lng.toString();
+}
+
+/** Get distance in miles between 2 zip codes. */
+async function getDistance(zip1, zip2) {
+  const resp = await fetch(`https://www.mapquestapi.com/directions/v2/route?key=${process.env.MAPQUEST_KEY}&from=${zip1}&to=${zip2}`);
+  return (await resp.json()).route.distance;
+}
+
+/** Check if user is within radius. */
+async function checkRadius(zip1, zip2, radius) {
+  const resp = await fetch(`https://www.mapquestapi.com/directions/v2/route?key=${process.env.MAPQUEST_KEY}&from=${zip1}&to=${zip2}`);
+  const data = (await resp.json()).route.distance;
+
+  return data <= radius;
 }
 
 module.exports = {
