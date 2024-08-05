a74a1d4268c38d4111d6f00f77726b0252f60983
Elise Ngo <elisetvy@gmail.com>
2024-08-05 16:47:03 -0700

    Remove comment

--- friender-seed.sql
+++ friender-seed.sql
@@ -1,12 +1,5 @@
 -- Seed database with users in different locations.
 
--- pinky
--- brain
--- yzma
--- kirk
--- michael (of tien and michael)
--- theresa
-
 INSERT INTO users (username, password, name, email, dob, photo, zip, latlng, radius, bio)
 VALUES ('snooba',
         '$2b$12$kqxU1kB58bb4q05U0wAuD.akGDdAJSozxQTrAQsYS.sasz5Ph/TdK',
